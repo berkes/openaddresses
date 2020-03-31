43e09e4f4cc6019c04216d8f956dcfaca154f641
ingalls <nick@ingalls.ca>
2020-03-30 21:19:46 -0700

    Run source tests

--- test/sources_validator.js
+++ test/sources_validator.js
@@ -16,32 +16,29 @@ ajv.addMetaSchema(require('./geojson.json'), "http://json.schemastore.org/geojso
 const validate = ajv.compile(schema);
 const validate_v2 = ajv.compile(schema_v2);
 
-function testAllSources() {
-    // find all the sources, has to be synchronous for tape
-    glob.sync('sources/**/*.json').forEach((source) => {
-        tape(`tests for ${source}`, (test) => {
-            test.test(`schema-validation for source ${source}`, (t) => {
-                try {
-                    const data = JSON.parse(fs.readFileSync(source, 'utf8'));
+// find all the sources, has to be synchronous for tape
+glob.sync('sources/**/*.json').forEach((source) => {
+    tape(`tests for ${source}`, (test) => {
+        test.test(`schema-validation for source ${source}`, (t) => {
+            try {
+                const data = JSON.parse(fs.readFileSync(source, 'utf8'));
 
-                    let valid = true;
-                    if (data.schema && data.schema === '2') {
-                        valid = validate_v2(data);
-                    } else {
-                        valid = validate(data);
-                    }
-
-                    t.ok(valid, `${source}: ${JSON.stringify(validate.errors)}`);
-
-                } catch (err) {
-                    t.fail(`could not parse ${source} as JSON: ${err}`);
+                let valid = true;
+                if (data.schema && data.schema === 2) {
+                    valid = validate_v2(data);
+                } else {
+                    valid = validate(data);
                 }
 
-                t.end();
+                t.ok(valid, `${source}: ${JSON.stringify(validate.errors)}`);
 
-            });
+            } catch (err) {
+                t.fail(`could not parse ${source} as JSON: ${err}`);
+            }
+
+            t.end();
 
         });
 
     });
-}
+});
