48ad45b0c73205457c1bfe4ff6ed7a45011d25a8
ingalls <nick@ingalls.ca>
2020-03-30 21:24:31 -0700

    Fix schema issues

--- test/sources_validator.js
+++ test/sources_validator.js
@@ -18,27 +18,25 @@ const validate_v2 = ajv.compile(schema_v2);
 
 // find all the sources, has to be synchronous for tape
 glob.sync('sources/**/*.json').forEach((source) => {
-    tape(`tests for ${source}`, (test) => {
-        test.test(`schema-validation for source ${source}`, (t) => {
-            try {
-                const data = JSON.parse(fs.readFileSync(source, 'utf8'));
+    tape(`tests for ${source}`, (t) => {
+        try {
+            const data = JSON.parse(fs.readFileSync(source, 'utf8'));
 
-                let valid = true;
-                if (data.schema && data.schema === 2) {
-                    valid = validate_v2(data);
-                } else {
-                    valid = validate(data);
-                }
+            let valid = true;
+            if (data.schema && data.schema === 2) {
+                valid = validate_v2(data);
+
+                t.ok(valid, `${source}: ${JSON.stringify(validate_v2.errors)}`);
+            } else {
+                valid = validate(data);
 
                 t.ok(valid, `${source}: ${JSON.stringify(validate.errors)}`);
-
-            } catch (err) {
-                t.fail(`could not parse ${source} as JSON: ${err}`);
             }
 
-            t.end();
+        } catch (err) {
+            t.fail(`could not parse ${source} as JSON: ${err}`);
+        }
 
-        });
-
+        t.end();
     });
 });
